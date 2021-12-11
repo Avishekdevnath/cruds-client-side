a10c5150c1107e9c8829225c4c7703c63cb0f1bb
Avishekdevnath <avishekdevnath@gmail.com>
2021-12-11 15:56:47 +0600

    A

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataTable from '../DataTable/DataTable';
+
+const Home = () => {
+    return (
+        <div>
+            <DataTable></DataTable>
+        </div>
+    );
+};
+
+export default Home;
