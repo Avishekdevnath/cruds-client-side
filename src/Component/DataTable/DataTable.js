1921e0589c25062767dbf52da421f734b56b61f3
SujoyDevnath <sujoydevnathdipta@gmail.com>
2021-12-11 14:32:19 +0600

    s

---
+++ src/Component/DataTable/DataTable.js
@@ -0,0 +1,98 @@
+import { Box } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { Button, FormControlLabel, TextField } from '@mui/material';
+
+const DataTable = () => {
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Name', width: 190 },
+        {
+            field: 'phone', headerName: 'Phone Number', width: 150
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            width: 240,
+        },
+        {
+            field: 'hobbies',
+            headerName: 'Hobbies',
+            description: 'This column has a value getter and is not sortable.',
+            sortable: false,
+            width: 160,
+            // valueGetter: (params) =>
+            //     `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
+            //     }`,
+        },
+        {
+            field: "update",
+            headerName: "Update",
+            sortable: false,
+            renderCell: (params) => {
+                const onClick = (e) => {
+                    e.stopPropagation(); // don't select this row after clicking
+
+                    const api: GridApi = params.api;
+                    const thisRow: Record<string, GridCellValue> = {};
+
+                    api
+                        .getAllColumns()
+                        .filter((c) => c.field !== "__check__" && !!c)
+                        .forEach(
+                            (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+                        );
+
+                    return alert(JSON.stringify(thisRow, null, 4));
+                };
+
+                return <Button variant="contained" sx={{ background: 'green' }} onClick={onClick}>Update</Button>;
+            }
+        },
+        {
+            field: "delete",
+            headerName: "Delete",
+            sortable: false,
+            renderCell: (params) => {
+                const onClick = (e) => {
+                    e.stopPropagation(); // don't select this row after clicking
+
+                    const api: GridApi = params.api;
+                    const thisRow: Record<string, GridCellValue> = {};
+
+                    api
+                        .getAllColumns()
+                        .filter((c) => c.field !== "__check__" && !!c)
+                        .forEach(
+                            (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+                        );
+
+                    return alert(JSON.stringify(thisRow, null, 4));
+                };
+
+                return <Button variant="contained" sx={{ background: 'red' }} onClick={onClick}>Delete</Button>;
+            }
+        }
+    ];
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
+    return (
+        <Box sx={{ height: 400, width: '100%', my: 5, mx: 'auto' }}>
+            <DataGrid
+                rows={users}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+            />
+        </Box>
+    );
+};
+
+export default DataTable;
